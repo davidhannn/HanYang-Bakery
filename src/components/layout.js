c58c74b614a70e0acdb81c83ae4d6383025bff96
David Han <email>
2020-09-17 20:25:02 -0700

    Navbar added and basic css

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,17 @@
 import React, { Fragment } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import Navbar from "./Globals/Navbar"
 
 import "./bootstrap.min.css"
-import "./layout.css"
+import "./layout.scss"
 
-const Layout = ({ children }) => <Fragment>{children}</Fragment>
+const Layout = ({ children }) => (
+  <Fragment>
+    <Navbar />
+    {children}
+  </Fragment>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
