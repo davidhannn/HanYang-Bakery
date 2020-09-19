d13d28b5fdd41881e48fb1b429eb2c5bd539b53f
David Han <email>
2020-09-18 21:44:36 -0700

    Snipcart Setups

---
+++ src/components/Home/Contact.js
@@ -0,0 +1,58 @@
+import React from "react"
+import Title from "../Globals/Title"
+
+const Contact = () => {
+  return (
+    <section className="contact py-5">
+      <Title title="contact us" />
+      <div className="row">
+        <div className="col-10 col-sm-8 col-md-6 mx-auto">
+          <form action="https://formspree.io/don@carpusfood.com" method="POST">
+            {/* name */}
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                id="name"
+                placeholder="john smith"
+              />
+            </div>
+            {/* email */}
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                className="form-control"
+                name="email"
+                id="email"
+                placeholder="john smith"
+              />
+            </div>
+            {/* description */}
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <textarea
+                name="description"
+                id="description"
+                className="form-control"
+                rows="5"
+                placeholder="Enter Message"
+              ></textarea>
+            </div>
+            {/* Submit */}
+            <button
+              type="submit"
+              className="btn btn-yellow btn-block text-capitalize mt-5"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
